81c1b86fa1a8542770e7a618adfe69223c68d6f4
Alem Tuzlak <t.zlak@hotmail.com>
2024-07-17 14:06:27 +0200

    Switched to modern tools

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,6 +11,12 @@ export default defineConfig({
 			all: false,
 			provider: "v8",
 			reporter: ["json-summary", "html"],
+			thresholds: {
+				statements: 80,
+				branches: 80,
+				functions: 80,
+				lines: 80,
+			},
 		},
 	},
 })
